6544815140dbf8f48bbfa2cc328b2fa1892274e6
Niko Ahonen <n.p.ahonen@gmail.com>
2021-04-05 13:00:38 +0300

    serve static assets from cloudflare worker

--- cache-proxy/index.js
+++ cache-proxy/index.js
@@ -1,8 +1,32 @@
+import { getAssetFromKV } from "@cloudflare/kv-asset-handler"
+
 addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
+  event.respondWith(handleRequest(event));
 });
 
-async function handleRequest(request) {
+const handleRequest = async (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith('/api')) {
+    return await apiCall(event);
+  }
+  // static assets as fallback for all
+  return await handleStaticAssets(event);
+}
+
+const handleStaticAssets = async (event) => {
+  try {
+    return await getAssetFromKV(event);
+  } catch (e) {
+    let pathname = new URL(event.request.url).pathname
+    return new Response(`"${pathname}" not found`, {
+      status: 404,
+      statusText: "not found",
+    })
+  }
+}
+
+const apiCall = async (event) => {
+  const request = event.request;
   const url = new URL(request.url);
   url.hostname = "synnytyssairaala-haku.herokuapp.com";
   const r = new Request(url, request);
